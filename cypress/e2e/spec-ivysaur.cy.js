e396aa9fb36d86a0c711427933ce803b23a2a732
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2025-05-08 16:36:59 +0300

    Update and rename spec-copy-1.cy.js to spec-ivysaur.cy.js

---
+++ cypress/e2e/spec-ivysaur.cy.js
@@ -0,0 +1,14 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+  
+describe('Pokedex', function() => {
+  it('ivysaur was selected', function() => {
+    cy.visit('https://localhost:5000/ivysaur')
+    cy.contains('chlorophyll')
+  })
+})
